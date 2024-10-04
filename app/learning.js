d2e1ca288cc55bee78048ea58dc2f3156aba6c4c
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-04 15:51:48 +0600

    docs(learning):  add list of learned features and tasks completed from module

---
+++ app/learning.js
@@ -0,0 +1,23 @@
+/* 
+1. project setup ❌
+2. routing ✔
+3. nested routing ✔
+4. useRouter (Button Click Routing) ✔
+5. RootLayout ✔
+6. Dashboard Layout ✔
+7. meta data ✔
+8. active route ✔
+9. loading (Dashboard) ✔
+10. loading (root) ✔
+10. loading (specefic page loading) ✔
+11. loading (specefic component loading) ✔
+11. global-error ✔
+12. Error boundary (Root lavel) ✔
+13. Error boundary (Dashboard) ✔
+14. Custom Link component ✔
+15. prefetch (false,true) ✔
+16. route group ✔
+17. dynamic route ✔
+18. generateStaticParams ✔
+19. component structure
+*/
